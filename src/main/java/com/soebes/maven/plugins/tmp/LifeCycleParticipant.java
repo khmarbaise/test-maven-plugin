0c715b8701f93b49d66965ca130895add733017f
Karl Heinz Marbaise <khmarbaise@apache.org>
2015-10-12 12:44:38 +0200

    Changed.

--- src/main/java/com/soebes/maven/plugins/tmp/LifeCycleParticipant.java
+++ src/main/java/com/soebes/maven/plugins/tmp/LifeCycleParticipant.java
@@ -14,51 +14,75 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @Singleton
-@Named("lifeobserver")
-public class LifeCycleParticipant extends AbstractMavenLifecycleParticipant {
-	private final Logger LOGGER = LoggerFactory.getLogger(getClass());
+@Named
+public class LifeCycleParticipant
+    extends AbstractMavenLifecycleParticipant
+{
+    private final Logger LOGGER = LoggerFactory.getLogger( getClass() );
 
-	private RepositorySystem system;
+    private String conf;
 
-	public RepositorySystem getSystem() {
-		return system;
-	}
+    private RepositorySystem system;
 
-	public void setSystem(RepositorySystem system) {
-		this.system = system;
-	}
+    public RepositorySystem getSystem()
+    {
+        return system;
+    }
 
-	@Inject
-	public LifeCycleParticipant(RepositorySystem system) {
-		LOGGER.info("LifeCycleParticipant::LifeCycleParticipant(ctro) {}", this);
-		this.system = system;
-	}
+    public void setSystem( RepositorySystem system )
+    {
+        this.system = system;
+    }
 
-	@Override
-	public void afterProjectsRead(MavenSession session) {
-		LOGGER.info("LifeCycleParticipant::afterProjectsRead() {}", this);
-		LOGGER.info(" -> " + session.getCurrentProject().getId());
-	}
+    @Inject
+    public LifeCycleParticipant( RepositorySystem system )
+    {
+        LOGGER.info( "LifeCycleParticipant::LifeCycleParticipant(ctro) {}", this );
+        this.system = system;
+        this.conf = "Unkonwn";
+    }
 
-	@Override
-	public void afterSessionStart(MavenSession session) {
-		//This is only be called if this an extension only...
-		LOGGER.info("LifeCycleParticipant::afterSessionStart() {}", this);
-	}
+    @Override
+    public void afterProjectsRead( MavenSession session )
+    {
+        LOGGER.info( "LifeCycleParticipant::afterProjectsRead() {}", this );
+        LOGGER.info( " -> " + session.getCurrentProject().getId() );
+    }
 
-	@Override
-	public void afterSessionEnd(MavenSession session) throws MavenExecutionException {
-		LOGGER.info("LifeCycleParticipant::afterSessionEnd(start) {}", this);
-		MavenProject project = session.getProjects().get(0);
+    @Override
+    public void afterSessionStart( MavenSession session )
+    {
+        //This is only be called if this an extension only...
+        LOGGER.info( "LifeCycleParticipant::afterSessionStart() {}", this );
+    }
 
-		if (!project.hasLifecyclePhase(LifecyclePhase.DEPLOY.id())) {
-			return;
-		}
+    @Override
+    public void afterSessionEnd( MavenSession session )
+        throws MavenExecutionException
+    {
+        LOGGER.info( "LifeCycleParticipant::afterSessionEnd(start) {}", this );
+        MavenProject project = session.getProjects().get( 0 );
 
-		for (MavenProject p : session.getProjectDependencyGraph().getSortedProjects()) {
-			LOGGER.info("-> Project:" + p.getId());
-		}
-		LOGGER.info("LifeCycleParticipant::afterSessionEnd(end) {}", this);
-	}
+        if ( !project.hasLifecyclePhase( LifecyclePhase.DEPLOY.id() ) )
+        {
+            return;
+        }
+
+        for ( MavenProject p : session.getProjectDependencyGraph().getSortedProjects() )
+        {
+            LOGGER.info( "-> Project:" + p.getId() );
+        }
+        LOGGER.info( "LifeCycleParticipant::afterSessionEnd(end) {}", this );
+    }
+
+    public String getConf()
+    {
+        return conf;
+    }
+
+    public void setConf( String conf )
+    {
+        this.conf = conf;
+    }
 
 }
--- src/main/java/com/soebes/maven/plugins/tmp/TestMojo.java
+++ src/main/java/com/soebes/maven/plugins/tmp/TestMojo.java
@@ -7,28 +7,24 @@ import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
-import org.apache.maven.project.MavenProject;
 
 /**
  * Will check dependencies of your project and fail the build if they are not up-to-date.
  * 
  * @author Karl-Heinz Marbaise <a href="mailto:khmarbaise@apache.org">khmarbaise@apache.org</a>
  */
-@Mojo( name = "test", defaultPhase = LifecyclePhase.NONE, requiresProject = true, threadSafe = true )
+@Mojo(name = "test", defaultPhase = LifecyclePhase.NONE, requiresProject = true, threadSafe = true)
 public class TestMojo
     extends AbstractUpToDateMojo
 {
 
-	@Parameter(defaultValue = "${project}", required = true, readonly = true) 
-	private MavenProject project;
-	
-	@Parameter
-	private String conf;
-	
-	@Inject
-	private LifeCycleParticipant deploy;
-	
-	public void execute()
+    @Parameter
+    private String conf;
+
+    @Inject
+    private LifeCycleParticipant deploy;
+
+    public void execute()
         throws MojoExecutionException, MojoFailureException
     {
 
@@ -38,8 +34,8 @@ public class TestMojo
             return;
         }
 
-//        deploy.setConf(conf);
+        getLog().info( "Participant: " + deploy );
+        deploy.setConf( conf );
     }
-
 
 }
